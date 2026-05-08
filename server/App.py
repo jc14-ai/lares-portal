a6337966ba05acd23302a64736345d936a84719d
jc14-ai <jestalycastillo15@gmail.com>
2026-05-08 10:40:20 +0800

    Add: lares portal server side rendering using gsheet API

---
+++ server/App.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+
+CORS(app)
+
+@app.route("/")
+def home():
+    return jsonify({"message": "Lares Portal Server running."})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001, debug=True)
+    
